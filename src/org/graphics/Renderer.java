59a06e7faba488e9445ebf082d770a2b65b92fb3
Jbern16 <jbern16@gmail.com>
2016-07-05 17:18:52 -0600

    add dyanamic window size

--- src/org/graphics/Renderer.java
+++ src/org/graphics/Renderer.java
@@ -3,6 +3,7 @@ package org.graphics;
 import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -16,7 +17,36 @@ public class Renderer {
 	private static int canvasWidth = 0;
 	private static int canvasHeight = 0;
 	
+	private static final int GAME_WIDTH = 400;
+	private static final int GAME_HEIGHT = 250;
+	
+	private static int gameWidth = 0;
+	private static int gameHeight = 0;
+	
+	private static void getBestSize()  {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		
+		Dimension screenSize = toolkit.getScreenSize();
+		
+		boolean done = false;
+		while(!done) {
+			canvasWidth += GAME_WIDTH;
+			canvasHeight += GAME_HEIGHT;
+			
+			if(canvasWidth > screenSize.width || canvasHeight > screenSize.height) {
+				canvasWidth -= GAME_WIDTH;
+				canvasHeight -= GAME_HEIGHT;
+				
+				done = true;
+			}
+		}
+		int xDiff = screenSize.width - canvasWidth;
+		int yDiff = screenSize.height - canvasHeight;
+	}	
+	
 	public static void init() {
+		getBestSize();
+		
 		frame = new Frame();
 		canvas = new Canvas();
 		
@@ -31,6 +61,8 @@ public class Renderer {
 				Game.quit();
 			}
 		});
+		
+		
 		
 		frame.setVisible(true);
 	}
