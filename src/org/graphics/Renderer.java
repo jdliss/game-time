80c6e764cd083e02272a98f641461ae468744e63
Jbern16 <jbern16@gmail.com>
2016-07-05 18:23:23 -0600

    add fullscreen, stuck on rending the rect

--- src/org/graphics/Renderer.java
+++ src/org/graphics/Renderer.java
@@ -1,11 +1,17 @@
 package org.graphics;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.VolatileImage;
 
 import org.game.Game;
 
@@ -20,6 +26,9 @@ public class Renderer {
 	private static final int GAME_WIDTH = 400;
 	private static final int GAME_HEIGHT = 250;
 	
+	private static int gameHeight = 0;
+	private static int gameWidth = 0;
+	
 	private static void getBestSize()  {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		
@@ -29,15 +38,53 @@ public class Renderer {
 		while(!done) {
 			canvasWidth += GAME_WIDTH;
 			canvasHeight += GAME_HEIGHT;
-			
 			if(canvasWidth > screenSize.width || canvasHeight > screenSize.height) {
-				canvasWidth -= GAME_WIDTH + GAME_WIDTH / 10 ;
-				canvasHeight -= GAME_HEIGHT + GAME_HEIGHT / 10 ;
-				
+				canvasWidth -= GAME_WIDTH;
+				canvasHeight -= GAME_HEIGHT;
+				gameHeight = canvasHeight;
+				gameWidth = canvasWidth;
 				done = true;
 			}
 		}
+		int xDiff = screenSize.width - canvasWidth;
+		int yDiff = screenSize.height - canvasHeight;
+		int factor = canvasWidth / GAME_WIDTH;
+		gameWidth = canvasWidth / factor + xDiff / factor;
+		gameHeight = canvasHeight / factor + yDiff / factor;
+		canvasWidth = gameWidth * factor;
+		canvasHeight = gameHeight * factor;
 	}	
+	
+	private static void startRendering() {
+		Thread thread = new Thread() {
+			public void run() {
+				GraphicsConfiguration gc = canvas.getGraphicsConfiguration();
+				VolatileImage vImage = gc.createCompatibleVolatileImage(gameWidth, gameHeight);
+				while(true) {
+					if (vImage.validate(gc) == VolatileImage.IMAGE_INCOMPATIBLE) {
+						vImage = gc.createCompatibleVolatileImage(gameWidth, gameHeight);
+					}
+					Graphics g = vImage.getGraphics();
+					g.setColor(Color.black);
+					g.fillRect(0, 0, gameWidth, gameHeight);
+					g.dispose();
+					g = canvas.getGraphics();
+					g.drawImage(vImage, 0, 0, canvasWidth, canvasHeight, null);
+					g.dispose();
+				
+				}
+			}
+		};
+	}
+	
+	private static void makeFullscreen() {
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = env.getDefaultScreenDevice();
+		if (gd.isFullScreenSupported()) {
+			frame.setUndecorated(true);
+			gd.setFullScreenWindow(frame);
+		}
+	}
 	
 	public static void init() {
 		getBestSize();
@@ -47,9 +94,11 @@ public class Renderer {
 		
 		canvas.setPreferredSize(new Dimension(canvasWidth, canvasHeight));
 		frame.add(canvas);
+		makeFullscreen();
 		frame.pack();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
+		
 		
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -58,5 +107,8 @@ public class Renderer {
 		});
 		
 		frame.setVisible(true);
+		startRendering();
 	}
+
+
 }
